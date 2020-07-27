f131134ef7c22a13a8f82cfded841ce1c01a29c8
yubin <itqkrdbqls1001@naver.com>
2020-07-28 00:36:20 +0900

    유저 고정 특성 필드 추가. migration 안 함.

--- account/models.py
+++ account/models.py
@@ -31,6 +31,54 @@ class Customer(AbstractUser):
     # 문방구 위치 경도
     longitude = models.DecimalField(max_digits=10, decimal_places=6, default=0, verbose_name="문방구 위치 경도")
 
+
+    # 유저 특성(고정)
+    AGE = (
+        ("10s", "10"),
+        ("20s", "20"),
+        ("30s", "30"),
+        ("40s", "40"),
+        ("50s", "50"),
+        ("60s", "60"),
+        ("etcs", "기타")
+    )
+    age = models.CharField(max_length=5, blank=False, null=False, choices=AGE)
+
+    JOB = (
+        ("J1", "학생"),
+        ("J2", "직장인"),
+        ("J3", "사업가"),
+        ("J4", "기타"),
+    )
+    job = models.CharField(max_length=10, blank=False, null=False, choices=JOB)
+
+
+    USAGE = (
+        ("U1", "그림그리기"),
+        ("U2", "문서작성"),
+        ("U3", "다이어리 꾸미기"),
+        ("U4", "필기(공부 혹은 시험)"),
+        ("U5", "기타"),
+    )
+    usage = models.CharField(max_length=20, blank=False, null=False, choices=USAGE)
+
+    # PEN = (
+    #     ("P1", "형광펜"),
+    #     ("P2", "유성펜"),
+    #     ("P3", "볼펜"),
+    #     ("P4", "샤프펜슬"),
+    #     ("P5", "기타"),
+    # )
+    # 이 경우 펜을 복수선택할 수 있게 하려면..
+    # 1. 가입 이후 ManyToManyField를 이용해 따로 설정한다.
+    # 2. 최대 관심 펜 종류를 정해놓고 고정 ForeignKey? 를 설정한다.
+    #    -> 이때 기본값을 '기타'로 놓는 방법으로 값이 채워지지 않는 경우가 없도록 한다.
+    #       (홈페이지에서 유저의 리뷰를 볼 때 '기타'는 표시되지 않도록 한다.)
+    
+
+    # 유저 태그(추가)
+    tags = models.ManyToManyField('account.CustomerTag', blank=True)
+
     def __str__(self):
         if self.is_Customer:
             typeMarker = "[C]"
@@ -42,3 +90,11 @@ class Customer(AbstractUser):
             typeMarker = "[UNKNOWN]"
         
         return f"{typeMarker} {self.username}"
+
+
+class CustomerTag(models.Model):
+    class Meta:
+        verbose_name = "유저 태그"
+
+    targetCustomer = models.ManyToManyField(Customer)
+    tagBody = models.CharField(max_length=10)
