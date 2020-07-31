dcfc62fc6dec6d1d41e1ad67da25be69b6c6b3a0
yubin <itqkrdbqls1001@naver.com>
2020-07-31 06:22:21 +0000

    pythonanywhere 배포 준비

--- LionStore/settings.py
+++ LionStore/settings.py
@@ -13,8 +13,7 @@ SECRET_KEY = 'y^g-xodgruv0ep7fly4w726r&dgvuzy8y%9564(#5q!l(ug1_5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOST = ['.pythonanywhere.com','localhost','127.0.0.1']
 
 
 # Application definition
