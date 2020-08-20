c079f634e09a1a55676e74c185960a404d9ce6d2
hyeonsoolee <dlgustn3413@kookmin.ac.kr>
2020-08-21 00:07:10 +0900

    커밋 테스트

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lee hyeonsoo\desktop\lionstore_hs\lionstore\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
