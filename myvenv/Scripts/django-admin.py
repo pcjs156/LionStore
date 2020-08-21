7e0c8c06daff51ddab28da46b2fd4d9a50c57f25
pcjs156 <itqkrdbqls1001@naver.com>
2020-08-21 13:22:37 +0000

    myvenv 삭제

--- myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\lee hyeonsoo\desktop\lionstore_hs\lionstore\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
